98c69f1f57119d60f6014601914ea38a5bb6c9de
zeter17 <turok.w@gmail.com>
2023-08-03 01:11:29 +0300

    vase types

--- __init__.py
+++ __init__.py
@@ -1,28 +1,18 @@
-bl_info = {
-    "name": "Vase Generator",
-    "author": "OpenAI",
-    "version": (1, 0),
-    "blender": (3, 6, 1),
-    "location": "View3D > Add > Mesh > Add Vase",
-    "description": "Adds a new Vase Mesh Object",
-    "category": "Add Mesh",
-}
-
 import bpy
 import bmesh
 import numpy as np
+from scipy.interpolate import CubicSpline
 
 class VaseProperties(bpy.types.PropertyGroup):
     vase_types = [
-        ("TYPE_1", "Type 1", "Standard Vase"),
-        ("TYPE_2", "Type 2", "Thinner Vase with Pronounced Waist"),
-        ("TYPE_3", "Type 3", "Vase with Bulbous Bottom and Narrow Top"),
+        ("TYPE_1", "Type 1", "Wabi-Sabi Vase 1"),
+        ("TYPE_2", "Type 2", "Wabi-Sabi Vase 2"),
+        ("TYPE_3", "Type 3", "Wabi-Sabi Vase 3"),
     ]
 
     vase_type: bpy.props.EnumProperty(name="Vase Type", items=vase_types, default="TYPE_1")
     segments: bpy.props.IntProperty(name="Segments", default=100, min=3, max=1000)
     height: bpy.props.FloatProperty(name="Height", default=2.0, min=0.1, max=10.0)
-    thinness: bpy.props.FloatProperty(name="Thinness", default=0.5, min=0.1, max=2.0)
 
 class MESH_OT_add_vase(bpy.types.Operator):
     bl_idname = "mesh.add_vase"
@@ -37,22 +27,31 @@ class MESH_OT_add_vase(bpy.types.Operator):
         if "vase" in bpy.data.objects:
             bpy.data.objects.remove(bpy.data.objects["vase"], do_unlink=True)
 
+        # Define the vase profiles
+        vase_profiles = {
+            "TYPE_1": [(0.0, 0.2), (0.5, 1.0), (1.0, 0.8), (1.5, 0.6), (2.0, 0.4)],
+            "TYPE_2": [(0.0, 0.8), (1.0, 0.7), (2.0, 0.6)],
+            "TYPE_3": [(0.0, 0.2), (1.0, 1.0), (2.0, 0.2)],
+        }
+
+        # Get the vase profile
+        profile = vase_profiles[props.vase_type]
+
+        # Normalize the vase height
+        profile = [(z / profile[-1][0] * props.height, r) for z, r in profile]
+
+        # Interpolate the vase profile
+        z_points, r_points = zip(*profile)
+        spline = CubicSpline(z_points, r_points)
+        z = np.linspace(0, props.height, props.segments)
+        r = spline(z)
+
         # Define the parameters for the vase
-        u = np.linspace(0, 2 * np.pi, props.segments)  # u parameter
-        v = np.linspace(-props.height, props.height, props.segments)  # v parameter
-        u, v = np.meshgrid(u, v)
+        theta = np.linspace(0, 2 * np.pi, props.segments)  # theta parameter
+        z, theta = np.meshgrid(z, theta)
 
-        # Define the parametric equations for the vase
-        if props.vase_type == "TYPE_1":
-            r = props.thinness + 0.1 * np.cos(np.pi * v)  # radius
-        elif props.vase_type == "TYPE_2":
-            r = props.thinness + 0.05 * np.cos(2 * np.pi * v)  # radius
-        elif props.vase_type == "TYPE_3":
-            r = props.thinness + 0.1 * (1 + np.cos(np.pi * v))  # radius
-
-        x = r * np.cos(u)
-        y = r * np.sin(u)
-        z = v
+        x = r * np.cos(theta)
+        y = r * np.sin(theta)
 
         # Create a new mesh and link it to the scene
         mesh = bpy.data.meshes.new("vase")
@@ -97,7 +96,6 @@ class VASE_PT_vase_panel(bpy.types.Panel):
         layout.prop(props, "vase_type")
         layout.prop(props, "segments")
         layout.prop(props, "height")
-        layout.prop(props, "thinness")
 
         # Draw the "Add Vase" button
         layout.operator(MESH_OT_add_vase.bl_idname)
